8bfc883fbb24eab1f0f11a3100d4a8bc7f8e0c25
Taninwat <fame5454@gmail.com>
2023-10-31 18:57:15 +0700

    Fix not found page

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,154 +2,101 @@
   <div class="bg-gray-800">
     <nav class="flex items-center justify-between bg-FFC288 flex-wrap p-6">
       <div class="flex items-center flex-shrink-0 text-black mr-6">
-        <span class="font-semibold text-xl tracking-tight"
-          ><img
-            src="../components/image/Screenshot_2566-10-21_at_15.33.09-transformed.png"
-            alt="Logo"
-            class="h-10"
-        /></span>
+        <span class="font-semibold text-xl tracking-tight"><img
+            src="../components/image/Screenshot_2566-10-21_at_15.33.09-transformed.png" alt="Logo" class="h-10" /></span>
       </div>
       <div class="block lg:hidden">
-        <button
-          @click="isOpen = !isOpen"
-          class="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-200 hover:text-white hover:border-white"
-        >
-          <svg
-            class="fill-current h-3 w-3"
-            :class="{ hidden: isOpen, block: !isOpen }"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
+        <button @click="isOpen = !isOpen"
+          class="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-200 hover:text-white hover:border-white">
+          <svg class="fill-current h-3 w-3" :class="{ hidden: isOpen, block: !isOpen }" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20">
             <path d="M0 4h20v2H0V4zm0 5h20v2H0V9zm0 5h20v2H0v-2z" />
           </svg>
-          <svg
-            class="fill-current h-3 w-3"
-            :class="{ hidden: !isOpen, block: isOpen }"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
+          <svg class="fill-current h-3 w-3" :class="{ hidden: !isOpen, block: isOpen }" xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20">
             <title>Close</title>
             <path
-              d="M14.348 14.849L10 10.5l4.348-4.35l-1.415-1.414L8.582 9.086L4.231 4.736L2.817 6.15l4.35 4.348l-4.35 4.349l1.414 1.415l4.351-4.35l4.349 4.35l1.415-1.415z"
-            />
+              d="M14.348 14.849L10 10.5l4.348-4.35l-1.415-1.414L8.582 9.086L4.231 4.736L2.817 6.15l4.35 4.348l-4.35 4.349l1.414 1.415l4.351-4.35l4.349 4.35l1.415-1.415z" />
           </svg>
         </button>
       </div>
-      <div
-        :class="{ hidden: !isOpen, block: isOpen }"
-        class="w-full flex-grow lg:flex lg:items-center lg:w-auto"
-      >
+      <div :class="{ hidden: !isOpen, block: isOpen }" class="w-full flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
           <div class="flex flex-col lg:flex-row">
-            <li
-              v-if="authStore.userRole == 'ROLE_ADMIN'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_ADMIN'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/about">About</RouterLink>
             </li>
-            <li
-              v-if="authStore.userRole == 'ROLE_ADMIN'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_ADMIN'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/student">Student</RouterLink>
             </li>
-            <li
-              v-if="authStore.userRole == 'ROLE_ADMIN'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_ADMIN'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/advisor">Advisor</RouterLink>
             </li>
-            <li
-              v-if="authStore.userRole == 'ROLE_ADMIN'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_ADMIN'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/announcement">Announcement</RouterLink>
             </li>
           </div>
           <div class="flex flex-col lg:flex-row">
-            <li
-              v-if="authStore.userRole == 'ROLE_STUDENT'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_STUDENT'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/about">About</RouterLink>
             </li>
-            <li
-              v-if="authStore.userRole == 'ROLE_STUDENT'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_STUDENT'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/student-profile">Student</RouterLink>
             </li>
-            <li
-              v-if="authStore.userRole == 'ROLE_STUDENT'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_STUDENT'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/announcement">Announcement</RouterLink>
             </li>
           </div>
           <div class="flex flex-col lg:flex-row">
-            <li
-              v-if="authStore.userRole == 'ROLE_ADVISOR'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_ADVISOR'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/about">About</RouterLink>
             </li>
-            <li
-              v-if="authStore.userRole == 'ROLE_ADVISOR'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_ADVISOR'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/advisor-profile">Advisor</RouterLink>
             </li>
-            <li
-              v-if="authStore.userRole == 'ROLE_ADVISOR'"
-              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-            >
+            <li v-if="authStore.userRole == 'ROLE_ADVISOR'"
+              class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
               <RouterLink to="/announcement">Announcement</RouterLink>
             </li>
           </div>
         </div>
 
         <div class="text-sm mx-4">
-          <p
-            v-if="authStore.userRole == 'ROLE_ADMIN'"
-            class="text-red-500 font-bold"
-          >
+          <p v-if="authStore.userRole == 'ROLE_ADMIN'" class="text-red-500 font-bold">
             ADMIN
           </p>
-          <p
-            v-if="authStore.userRole == 'ROLE_STUDENT'"
-            class="text-blue-500 font-bold"
-          >
+          <p v-if="authStore.userRole == 'ROLE_STUDENT'" class="text-blue-500 font-bold">
             STUDENT
           </p>
-          <p
-            v-if="authStore.userRole == 'ROLE_ADVISOR'"
-            class="text-green-500 font-bold"
-          >
+          <p v-if="authStore.userRole == 'ROLE_ADVISOR'" class="text-green-500 font-bold">
             ADVISOR
           </p>
         </div>
 
         <div class="text-sm">
-          <li
-            v-if="authStore.userRole == 'ROLE_ADMIN'"
-            class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-          >
+          <li v-if="authStore.userRole == 'ROLE_ADMIN'"
+            class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
             <a href="/" class="nav-link hover:cursor-pointer" @click="logout">
               LogOut
             </a>
           </li>
-          <li
-            v-if="authStore.userRole == 'ROLE_STUDENT'"
-            class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-          >
+          <li v-if="authStore.userRole == 'ROLE_STUDENT'"
+            class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
             <a href="/" class="nav-link hover:cursor-pointer" @click="logout">
               LogOut
             </a>
           </li>
-          <li
-            v-if="authStore.userRole == 'ROLE_ADVISOR'"
-            class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center"
-          >
+          <li v-if="authStore.userRole == 'ROLE_ADVISOR'"
+            class="font-dm mb-2 lg:mb-0 lg:mr-4 hover:bg-white p-3 rounded-md flex items-center">
             <a href="/" class="nav-link hover:cursor-pointer" @click="logout">
               LogOut
             </a>
@@ -186,6 +133,8 @@ if (token && userRole) {
 } else {
   authStore.logout();
 }
+
+
 </script>
 
 <script lang="ts">
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,17 +1,93 @@
-<script setup lang="ts">
-defineProps({
-  resource: {
-    type: String,
-    require: true,
-    default: 'page'
-  }
-})
-</script>
-
 <template>
   <div class="flex flex-col items-center mt-20">
-    <h1 class="text-4xl text-red-500 mb-4">Oops!</h1>
+    <h1 class="text-4xl text-red-500 mb-4 font-bold">Oops!</h1>
     <h3 class="text-lg text-gray-700 mb-8">The {{ resource }} you're looking for is not here.</h3>
-    <router-link :to="{ name: 'login' }" class="bg-65451F hover:bg-8D7B68 text-white py-2 px-4 rounded transition duration-300">Back to home page</router-link>
+    <button @click="logout" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+      Back to home page
+    </button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import { useMessageStore } from "@/stores/message";
+import { storeToRefs } from "pinia";
+import { useAuthStore } from "@/stores/auth";
+import { useRouter } from "vue-router";
+
+const isOpen = ref(false);
+const store = useMessageStore();
+const { message } = storeToRefs(store);
+const authStore = useAuthStore();
+const router = useRouter();
+const token = localStorage.getItem("access_token");
+const userRole = localStorage.getItem("user_role");
+const id = localStorage.getItem("id");
+
+function logout() {
+  authStore.logout();
+  router.push({ name: "login" });
+}
+
+if (token && userRole) {
+  authStore.reload(token, JSON.parse(userRole));
+} else {
+  authStore.logout();
+}
+</script>
+
+<style scoped>
+  .font-bold {
+    font-weight: bold;
+  }
+
+  .mt-20 {
+    margin-top: 5rem;
+  }
+
+  .text-4xl {
+    font-size: 2.25rem;
+  }
+
+  .text-lg {
+    font-size: 1.125rem;
+  }
+
+  .text-red-500 {
+    color: #DC2626;
+  }
+
+  .text-gray-700 {
+    color: #4B5563;
+  }
+
+  .bg-red-500 {
+    background-color: #FFC288;
+  }
+
+  .hover\:bg-red-700:hover {
+    background-color: #ffac5f;
+  }
+
+  .text-white {
+    color: #FFFFFF;
+  }
+
+  .py-2 {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+  }
+
+  .px-4 {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+
+  .rounded {
+    border-radius: 0.25rem;
+  }
+
+  .hover\:bg-white:hover {
+    background-color: #FFFFFF;
+  }
+</style>
